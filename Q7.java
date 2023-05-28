7bc0facac184e59baced22ccca07b89b4169ee74
rn_Xtremer_ <119094662+rnxtremer@users.noreply.github.com>
2023-05-29 01:12:26 +0530

    Update Q7.java

--- Q7.java
+++ Q7.java
@@ -1,43 +1,59 @@
-/*Define a base class Person with instance variable name, age. The instance variables are initialized 
-through constructors. The prototype of constructor is as below.Person (string, int)
-Define a derived class Employee with instance variables Eid, salary. The instance variables are initialized through constructors. 
-The prototype of constructor is as below.
-Employee (string, int, int, double). Another instance method of Employee class is empDisplay() to display the information of 
-employee details. */
-class Person
-{
-    String name;
-    int age;
-    public Person(String name, int age) 
-    {
+/*Define a base class Person with instance variable name, age. The instance variables are 
+initialized through constructors. The prototype of constructor is as below.
+Person (string, int)
+Define a derived class Employee with instance variables Eid, salary. The instance variables 
+are initialized through constructors. The prototype of constructor is as below.
+Employee (string, int, int, double). Another instance method of Employee class is empDisplay() 
+to display the information of employee details.*/
+
+import java.util.Scanner;
+
+class Person {
+    protected String name;
+    protected int age;
+
+    public Person(String name, int age) {
         this.name = name;
         this.age = age;
     }
 }
-class Employee extends Person 
-{
-    int Eid;
-    double salary;
-    public Employee(String name, int age, int Eid, double salary)
-    {
+
+class Employee extends Person {
+    private int Eid;
+    private double salary;
+
+    public Employee(String name, int age, int Eid, double salary) {
         super(name, age);
         this.Eid = Eid;
-        this.salary = salary; 
+        this.salary = salary;
     }
 
-    public void empDisplay() 
-    {
-        System.out.println(  name+"\t\t"+ age+"\t"+Eid+"\t"+salary);
+    public void empDisplay() {
+        System.out.println("Employee Details:");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Employee ID: " + Eid);
+        System.out.println("Salary: " + salary);
     }
 }
-public class Q7
- {
-    public static void main(String[] args) 
-    {
-        Employee emp1 = new Employee("Ansuman", 24, 72563, 83623.34);
-        Employee emp2 = new Employee("Chiku", 25, 85478, 63485.25);
-        System.out.println("Emp_Name\t" +"Age:\t" +"Emp_ID \t"+"Salary\t");
-        emp1.empDisplay();
-        emp2.empDisplay();
+
+public class Q7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Enter age: ");
+        int age = scanner.nextInt();
+
+        System.out.print("Enter Employee ID: ");
+        int eid = scanner.nextInt();
+
+        System.out.print("Enter salary: ");
+        double salary = scanner.nextDouble();
+
+        Employee employee = new Employee(name, age, eid, salary);
+        employee.empDisplay();
     }
 }
