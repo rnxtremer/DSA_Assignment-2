b60ad3aff0f7721214b2c8774a34ab8485112b4c
rn_Xtremer_ <119094662+rnxtremer@users.noreply.github.com>
2023-05-29 00:30:34 +0530

    Update Q2.java

--- Q2.java
+++ Q2.java
@@ -3,53 +3,53 @@ setData(), display(), add(). Write a Java program to add two complex numbers.
 The prototype of add method is: 
 public Complex add(Complex, Complex).*/
 
-import java.util.Scanner;
-class Complex
-{
-    double re;
-    double im;    
-    void setData (double r, double i) 
-    {
-        re = r;
-        im= i;
-    }
-    
-    void display() 
-    {
-        System.out.printf("%.2f %s %.2f%s\n",re,(im <0?" - ":" + ") ,Math.abs(im),"i");
-    }
-    
-    Complex add(Complex c1, Complex c2) {
-        Complex result = new Complex();
-        result.re = c1.re + c2.re;
-        result.im = c1.im + c2.im;
-        return result;
-    }     
-}
-public class Q2 
-{
-    public static void main(String[] args) 
-    {
-        Scanner obj=new Scanner(System.in);
-		double r,i;
+import java.util.*;
+public class Q2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		Complex c1=new Complex();
-		System.out.println("Enter real and imaginary part of 1st number separated by space :");
-		r = obj.nextDouble();
-        i = obj.nextDouble();
-		c1.setData(r,i);
 		Complex c2=new Complex();
-		System.out.println("Enter real and imaginary part of 2nd number separated by space :");
-		r = obj.nextDouble();
-        i = obj.nextDouble();
-		c2.setData(r,i);
-		System.out.println("The 1st Complex number is : ");
-		c1.display();
-		System.out.println("The 2nd Complex number is : ");
-		c2.display();
-		Complex sum=new Complex();
-		sum=sum.add(c1,c2);
-		System.out.println("The sum is : ");
-		sum.display();
-        obj.close();
-    }
+		
+		System.out.println("Enter the value of first complex number");
+		c1.setData();
+		System.out.println("Enter the value of second complex number");
+		c2.setData();
+		System.out.println("First complex number is:");
+		c1.Display();
+		System.out.println("Second complex number is:");
+		c2.Display();
+		Complex c3= new Complex();
+		c3=c3.add(c1,c2);
+		System.out.println("Sum of the two Complex Number is:");
+	    c3.Display();
+	}
+
+}
+class Complex{
+	Scanner sc=new Scanner(System.in);
+	int real;
+	int imag;
+	void setData()
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the real no.");
+		real=sc.nextInt();
+		System.out.println("Enter the imaginary no.");
+		imag=sc.nextInt();
+	}
+		void Display()
+		{
+			System.out.println("The complex no. is "+real+"+"+imag+"i");
+			
+		}
+		public Complex add(Complex c1,Complex c2)
+		{
+			Complex c=new Complex();
+			c.real=c1.real+c2.real;
+			c.imag=c1.imag+c2.imag;
+			return c;
+		
+	}
+	
 }
