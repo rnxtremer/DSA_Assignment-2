f9a6a09b2b8ca0f3772df7aa5b6cd9f0144f1d6f
rn_Xtremer_ <119094662+rnxtremer@users.noreply.github.com>
2023-05-29 01:48:48 +0530

    Update Q8.java

--- Q8.java
+++ Q8.java
@@ -1,3 +1,5 @@
+package Q8;
+
 /*Define an interface DetailInfo to declare methods display ( ) & count ( ). Another class Person
 contains a static data member maxcount, instance member name & method display ( ) to display 
 name of a person, count the no. of characters present in the name of the person.*/
