64497dda033bd3bee4c2f813c27ead33affebb5f
rn_Xtremer_ <119094662+rnxtremer@users.noreply.github.com>
2023-05-29 01:34:55 +0530

    Update Q8.java

--- Q8.java
+++ Q8.java
@@ -1,34 +1,54 @@
-/*Define an interface DetailInfo to declare methods display ( ) & count ( ). Another class Person 
-contains a static data member maxcount, instance member name & method display ( ) to display name
-of a person, count the no. of characters present in the name of the person. */
-interface DetailInfo 
-{
+/*Define an interface DetailInfo to declare methods display ( ) & count ( ). Another class Person
+contains a static data member maxcount, instance member name & method display ( ) to display 
+name of a person, count the no. of characters present in the name of the person.*/
+
+
+
+import java.util.Scanner;
+
+interface DetailInfo {
     void display();
     int count();
 }
-class Person implements DetailInfo 
-{
-    static int maxcount = 15;
-    String name;
-    public Person(String name)
-    {
+
+class Person implements DetailInfo {
+    private static int maxCount;
+    private String name;
+
+    public Person(String name) {
         this.name = name;
     }
-    public void display() 
-    {
+
+    public void display() {
         System.out.println("Name: " + name);
     }
-    public int count() 
-    {
+
+    public int count() {
         return name.length();
     }
-}
-class HelloWorld {
-    public static void main(String[] args) 
-    {
-        Person p = new Person("Ansuman");
-        p.display();
-        int count=p.count();
-        System.out.println("Name length: " + count);
+
+    public static void setMaxCount(int count) {
+        maxCount = count;
+    }
+
+    public static int getMaxCount() {
+        return maxCount;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter name: ");
+        String name = sc.nextLine();
+
+        Person person = new Person(name);
+        person.display();
+
+        int count = person.count();
+        System.out.println("Count: " + count);
+
+        Person.setMaxCount(count);
+        int maxCount = Person.getMaxCount();
+        System.out.println("Max Count: " + maxCount);
     }
 }
