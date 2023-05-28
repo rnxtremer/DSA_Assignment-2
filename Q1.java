508197885e19ac25ed40996c0e2c293cf679e4b7
rn_Xtremer_ <119094662+rnxtremer@users.noreply.github.com>
2023-05-29 00:29:32 +0530

    Update Q1.java

--- Q1.java
+++ Q1.java
@@ -1,45 +1,43 @@
-/*Question:
-Define a class Phone to store these three parts of a phone number separately as instance 
+/*Define a class Phone to store these three parts of a phone number separately as instance 
 variable (area_code, exchange, number). The class consists of two member methods: input ( )
 and display ( ). Create two class objects of type phone. Initialize one directly (by using dot (.) 
 operator and instance variable with value area_code: 212, exchange: 767, 
 number: 8900), and initialize other from the user through member method input ( ). Display 
-both the numbers.*/
+both the numbers*/
 
-class Phone 
-{
-    int area_code;
-    int exchange;
-    int number;
-    
-    void input(int a,int e,int n) 
-    {
-        area_code =a;
-        exchange = e;
-        number =n;
-    }
-    
-    void display() 
-    {
-        System.out.printf("(%03d) %03d-%04d", area_code, exchange,number);
-    }
+
+import java.util.Scanner;
+class Phone {
+	int Area_Code,Exchange,Number;
+	void input()
+	{
+		Scanner sc = new Scanner(System.in);
+	    Area_Code=sc.nextInt();
+		Exchange=sc.nextInt();
+		Number=sc.nextInt();
+	}
+	void Display()
+	{
+		System.out.println("("+Area_Code+")"+Exchange+"-"+Number);
+	}
 }
 public class Q1
 {
-    public static void main(String[] args) {
-        Phone phone1 = new Phone();
-        phone1.area_code=212;
-        phone1.exchange=767;
-        phone1.number=8900;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+Phone obj1=new Phone();
+Phone obj2=new Phone();
+System.out.println("Enter Area Code,Exchange and Number obj1");
+obj1.Area_Code=sc.nextInt();
+obj1.Exchange=sc.nextInt();
+obj1.Number=sc.nextInt();
+System.out.println("Enter Area Code,Exchange and Number obj2");
+obj2.input();
+System.out.println("My Number is :");
+obj1.Display();
+System.out.println("Your Number is :");
+obj1.Display();
+	}
 
-        Phone phone2 = new Phone();
-        phone2.input(415,555,1212);
-        
-        System.out.print("My number is ");
-        phone1.display();
-        
-        System.out.print("\nYour number is ");
-        phone2.display();
-    }
 }
-
