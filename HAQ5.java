1c15aa774f5117edfd447795d59db72384a7be87
rn_Xtremer_ <119094662+rnxtremer@users.noreply.github.com>
2023-06-02 10:04:52 +0530

    Update HAQ5.java

--- HAQ5.java
+++ HAQ5.java
@@ -1,3 +1,13 @@
+/*Create an interface RestaurantMeal that holds the name and price of a food item served by a 
+restaurant. The name is initialized as steak dinner and price is initialized as 600. Create an 
+interface HotelService that holds the name of the service, the service fee, and the room number 
+to which the service was supplied. Initialize the service as room service, service fee as Rs 24, 
+and room number as 1202. Create a RoomServiceMeal class that inherits from both 
+RestaurantMeal and HotelService. The RoomServiceMeal class consists of display method
+() to display the value of members of interface. Additionally, the display function should 
+display the total of the meals plus the room service fee. In a main () function, instantiate a 
+RoomServiceMeal object.*/
+
 class RestaurantMeal 
 {
     String name;
