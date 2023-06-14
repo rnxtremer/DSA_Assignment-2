ffd79a8222a54fab1cf2f01e2b32a67a8567cac0
rn_Xtremer_ <119094662+rnxtremer@users.noreply.github.com>
2023-06-14 11:38:25 +0530

    Update HAQ2.java

--- HAQ2.java
+++ HAQ2.java
@@ -1,3 +1,8 @@
+/*Define a class called Book with instance variables BName, BEdition, BPrice. Use constructor to 
+initialize the instance variables of the class. Another instance method display ( ) to display the book 
+information. A person purchase 5 different books. Display the book details which has the maximum 
+price. */
+
 class Book 
 {
     String BName;
