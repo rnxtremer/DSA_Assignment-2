bc51445cbfa8ce6d228f4b4b26c35a7f2d32fdec
rn_Xtremer_ <119094662+rnxtremer@users.noreply.github.com>
2023-06-14 19:12:16 +0530

    Update HAQ3.java

--- HAQ3.java
+++ HAQ3.java
@@ -4,40 +4,52 @@ definition include a parameterized constructor, display ( ) to display the insta
 objects and demonstrates the addition of two distances. */
 
 
+import java.util.Scanner;
+
 class Distance {
-    int meters;
-    int centimeters;
-    Distance(int meters, int centimeters) 
-    {
-        if (centimeters >= 100) 
-        {
-            meters += centimeters / 100;
-            centimeters = centimeters % 100;
-        }
+    private int meters;
+    private int centimeters;
+    
+    public Distance(int meters, int centimeters) {
         this.meters = meters;
         this.centimeters = centimeters;
-    }    
-    public void display()
-    {
-        System.out.println(meters + " m " + centimeters + " cm ");
-    }    
-    Distance sum(Distance d1, Distance d2) 
-    {
-        int me = d1.meters + d2.meters;
-        int cm = d1.centimeters + d2.centimeters;        
-        return new Distance(me, cm);
-    }
-}
-public class HW3{
-    public static void main(String[] args) {
-        Distance d1 = new Distance(5, 70);
-        Distance d2 = new Distance(3, 30);
-        System.out.println("Two distances are : ");
-        Distance d3 = d1.sum(d1, d2);
-        d1.display();
-        d2.display();
-        System.out.print("The sum is : ");
-        d3.display();
     }
     
+    public void display() {
+        System.out.println("Distance: " + meters + " meters, " + centimeters + " centimeters");
+    }
+    
+    public Distance sum(Distance d1, Distance d2) {
+        int totalMeters = d1.meters + d2.meters;
+        int totalCentimeters = d1.centimeters + d2.centimeters;
+        
+        if (totalCentimeters >= 100) {
+            totalMeters += totalCentimeters / 100;
+            totalCentimeters = totalCentimeters % 100;
+        }
+        
+        return new Distance(totalMeters, totalCentimeters);
+    }
 }
+
+public class HAQ3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Enter the distance for the first object (meters centimeters):");
+        int meters1 = scanner.nextInt();
+        int centimeters1 = scanner.nextInt();
+        Distance d1 = new Distance(meters1, centimeters1);
+        
+        System.out.println("Enter the distance for the second object (meters centimeters):");
+        int meters2 = scanner.nextInt();
+        int centimeters2 = scanner.nextInt();
+        Distance d2 = new Distance(meters2, centimeters2);
+        Distance sumDistance = d1.sum(d1, d2);
+        
+        d1.display();
+        d2.display();
+        sumDistance.display();
+    }
+}
+
