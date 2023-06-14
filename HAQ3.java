554e3227be540c114fff6c982d32d53c7997c906
rn_Xtremer_ <119094662+rnxtremer@users.noreply.github.com>
2023-06-14 19:09:57 +0530

    Update HAQ3.java

--- HAQ3.java
+++ HAQ3.java
@@ -1,3 +1,9 @@
+/*Write a Java code that would represent Distance object (meters, centimetres) using classes. The class 
+definition include a parameterized constructor, display ( ) to display the instance variables, sum 
+(Distance, Distance) to add two distances. Now write a main function that creates a couple of Distance 
+objects and demonstrates the addition of two distances. */
+
+
 class Distance {
     int meters;
     int centimeters;
